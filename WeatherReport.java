88ef00d0f82f0e9aa577d5cb59adcb9481463e89
JLMattix724 <30986934+JLMattix724@users.noreply.github.com>
2017-08-18 09:52:31 -0500

    Update WeatherReport.java

--- WeatherReport.java
+++ WeatherReport.java
@@ -1,11 +1,30 @@
-import java.util.Scanner;
-
-public class WeatherReport
+public class Report 
 {
 
 	public static void main(String[] args) 
 	{
+		Scanner cin = new Scanner (System.in);
 		
-	}
+		int F= cin.nextInt(); //F=Fahrenhiet
+		if (F<=45)
+		{
+			System.out.println("That's Freezing!");
+		}
+		else if (F<=75)
+		{
+			System.out.println("That's just right");
+		}
+		else if (F>=76)
+		{
+			System.out.println("That's too hot!");
+		}
+		double ans=0;
+		ans= ((F-32) * 5.0 / 9);
+		
+		{
+			System.out.println("The Temperature would be " + ans + " in Celsius");
+		}
+		cin.close();
+		}
 
 }
